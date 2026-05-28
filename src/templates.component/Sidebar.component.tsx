eb408e621c005688c175e4d7b739cc57c5c5ca0f
Miarotiana Stephan <miarotianastephan01@gmail.com>
2026-05-28 16:19:59 +0300

    Update Sidebar.component.tsx

--- src/templates.component/Sidebar.component.tsx
+++ src/templates.component/Sidebar.component.tsx
@@ -119,7 +119,7 @@ const Sidebar: React.FC<SidebarProps> = ({
       <div className="p-6 border-b border-gray-200 dark:border-gray-800">
         <div className={`flex items-center ${isCollapsed ? "justify-center" : "space-x-2"}`}>
           <span className="text-2xl font-bold text-gray-900 dark:text-white">
-            {isCollapsed ? 'V' : 'Victus'}
+            {isCollapsed ? 'V' : 'Victus RH'}
           </span>
           {!isCollapsed && (
             <span className="text-xl font-bold text-gray-500 dark:text-gray-400">&lt;V&gt;</span>
